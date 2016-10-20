3b29f3931dec62301d9362455f8de16eac5eb705
manikandanGlace <manikandan.n@glenwoodsystems.com>
2016-10-20 15:46:07 +0530

    Changed return type in import data controller
    Change-Id: I50bce549d6fa2366f1df9dade6e48eccb3c3b388

--- src/main/java/com/glenwood/glaceemr/server/application/controllers/ImportDataController.java
+++ src/main/java/com/glenwood/glaceemr/server/application/controllers/ImportDataController.java
@@ -1,43 +1,41 @@
 package com.glenwood.glaceemr.server.application.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.glenwood.glaceemr.server.application.services.chart.ImportData.ImportDataService;
+import com.glenwood.glaceemr.server.utils.EMRResponseBean;
 import com.google.common.base.Optional;
 import com.wordnik.swagger.annotations.Api;
-
-
 
 @Api(value = "Import", description = "Import Clinical Data ", consumes="application/json")
 @RestController
 @Transactional
 @RequestMapping(value="/user/ImportData.Action")
 public class ImportDataController {
-
-
 	@Autowired
 	ImportDataService importDataService;
-	
 	@RequestMapping(value="/ImportEncounterList",method=RequestMethod.GET)
 	@ResponseBody
-	public String getImportEncList(@RequestParam(value="patientId") Integer patientId,
+	public EMRResponseBean getImportEncList(@RequestParam(value="patientId") Integer patientId,
 			@RequestParam(value="encounterId") Integer encounterId) throws Exception{
-
-		patientId=Integer.parseInt(Optional.fromNullable(patientId+"").or("-1"));
-		encounterId=Integer.parseInt(Optional.fromNullable(encounterId+"").or("-1"));
-		return importDataService.getImportEncList(patientId,encounterId);
+		
+		    patientId=Integer.parseInt(Optional.fromNullable(patientId+"").or("-1"));
+		    encounterId=Integer.parseInt(Optional.fromNullable(encounterId+"").or("-1"));
+		    EMRResponseBean emrResponseBean=new EMRResponseBean();
+		    emrResponseBean.setData(importDataService.getImportEncList(patientId,encounterId));
+	return emrResponseBean;
 	}
 
 
 	@RequestMapping(value="/ImportData",method=RequestMethod.GET)
 	@ResponseBody
-	public void getImportData(@RequestParam(value="patientId") Integer patientId,
+	public void  getImportData(@RequestParam(value="patientId") Integer patientId,
 			@RequestParam(value="chartId") Integer chartId,
 			@RequestParam(value="encounterId") Integer encounterId,
 			@RequestParam(value="templateId") Integer templateId,
