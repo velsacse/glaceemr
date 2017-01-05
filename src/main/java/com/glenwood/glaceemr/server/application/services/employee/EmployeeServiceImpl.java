795e2d088a6ec0c94c2b2187b7a1591405e261a7
jeyanthkumars <jeyanthkumar@glenwoodsystems.com>
2017-01-05 15:50:00 +0530

    Usergroup employees added in forward popup.
    Change-Id: I9b0ffca816b18f4bfc028bc41ffb93161c4399c0

--- src/main/java/com/glenwood/glaceemr/server/application/services/employee/EmployeeServiceImpl.java
+++ src/main/java/com/glenwood/glaceemr/server/application/services/employee/EmployeeServiceImpl.java
@@ -1,17 +1,19 @@
 package com.glenwood.glaceemr.server.application.services.employee;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.glenwood.glaceemr.server.application.models.EmployeeProfile;
+import com.glenwood.glaceemr.server.application.models.UserGroup;
 import com.glenwood.glaceemr.server.application.repositories.EmployeeProfileRepository;
+import com.glenwood.glaceemr.server.application.repositories.UserGroupRepository;
 import com.glenwood.glaceemr.server.application.specifications.EmployeeSpecification;
 
 @Service
@@ -27,6 +29,9 @@ public class EmployeeServiceImpl implements EmployeeService {
 	@Autowired
 	EmployeeProfileRepository empProfileRepository;
 	
+	@Autowired
+	UserGroupRepository userGroupRepository;
+	
 	
 	/**
 	 * @param groupId 
@@ -35,15 +40,37 @@ public class EmployeeServiceImpl implements EmployeeService {
 	 */
 	@Override
 	public List<EmployeeProfile> getEmployeeDetails(String groupId, String sort) {
-		List<EmployeeProfile> employeeList=null;
-		if(groupId.equalsIgnoreCase("all"))
-		{
-			employeeList=empProfileRepository.findAll(EmployeeSpecification.getUsersList(sort));
+		List<EmployeeProfile> employeeList=new ArrayList<EmployeeProfile>();
+		List<EmployeeProfile> employees=null;
+		
+		List<Integer> groupidList=new ArrayList<Integer>();
+		
+		List<UserGroup> userGroup=userGroupRepository.findAll();
+		for (int i = 0; i < userGroup.size(); i++) {
+			if(!groupidList.contains(userGroup.get(i).getGroupid())){
+				
+			groupidList.add(userGroup.get(i).getGroupid());
+				
+			EmployeeProfile emp = new EmployeeProfile();
+			emp.setEmpProfileFullname(userGroup.get(i).getGroupname());
+			emp.setEmpProfileEmpid(userGroup.get(i).getGroupid());
+			emp.setEmpProfileGroupid(-100);
+			
+			employeeList.add(emp);
+			}
 		}
-		else
-		{
-			employeeList=empProfileRepository.findAll(EmployeeSpecification.getUsersList(groupId,sort));
+		if(groupId.equalsIgnoreCase("all")){
+			employees=empProfileRepository.findAll(EmployeeSpecification.getUsersList(sort));
 		}
+		else{
+			employees=empProfileRepository.findAll(EmployeeSpecification.getUsersList(groupId,sort));
+		}
+		
+		for (int i = 0; i < employees.size(); i++) {
+			employeeList.add(employees.get(i));
+		}
+		
+		
 		return employeeList;
 	}
 }
