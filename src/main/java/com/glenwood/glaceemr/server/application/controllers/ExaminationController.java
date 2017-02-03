298230b8322ba2d384dc55fa40af73be9a45e514
bhagyalakshmisabbella <bhagyalakshmi@glenwoodsystems.com>
2017-02-03 15:23:23 +0530

    Allergy,skintest,examination Audit log changes
    Change-Id: If86adb31e8f171920912d6e3ad084c3996fac6d7

--- src/main/java/com/glenwood/glaceemr/server/application/controllers/ExaminationController.java
+++ src/main/java/com/glenwood/glaceemr/server/application/controllers/ExaminationController.java
@@ -1,5 +1,7 @@
 package com.glenwood.glaceemr.server.application.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,6 +9,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.glenwood.glaceemr.server.application.services.audittrail.AuditTrailEnumConstants.LogActionType;
+import com.glenwood.glaceemr.server.application.services.audittrail.AuditTrailEnumConstants.LogModuleType;
+import com.glenwood.glaceemr.server.application.services.audittrail.AuditTrailEnumConstants.LogType;
+import com.glenwood.glaceemr.server.application.services.audittrail.AuditTrailEnumConstants.LogUserType;
+import com.glenwood.glaceemr.server.application.services.audittrail.AuditTrailEnumConstants.Log_Outcome;
+import com.glenwood.glaceemr.server.application.services.audittrail.AuditTrailSaveService;
 import com.glenwood.glaceemr.server.application.services.chart.Examination.ExaminationService;
 import com.glenwood.glaceemr.server.utils.EMRResponseBean;
 import com.google.common.base.Optional;
@@ -20,6 +28,12 @@ public class ExaminationController {
 
 	@Autowired
 	ExaminationService examinationService;
+	
+	@Autowired
+	AuditTrailSaveService auditTrailSaveService;
+	
+	@Autowired
+	HttpServletRequest request;
 
 	@RequestMapping(value="/ExaminationSystems",method=RequestMethod.GET)
 	@ResponseBody
@@ -43,6 +57,7 @@ public class ExaminationController {
 		System.out.println("======================================");
 		System.out.println("Total time taken:: "+(end-start)+"ms");
 		System.out.println("======================================");
+		auditTrailSaveService.LogEvent(LogType.GLACE_LOG, LogModuleType.PHYSICALEXAMINATION, LogActionType.VIEW, 0, Log_Outcome.SUCCESS, "successfully retrieved Examination system details", -1, request.getRemoteAddr(), patientId, "chartId="+chartId+"|encounterId"+encounterId+"|templateId="+templateId+"|clientId="+clientId, LogUserType.USER_LOGIN, "", "");
 		return emrResponseBean;
 	}
 	
@@ -70,6 +85,7 @@ public class ExaminationController {
 		System.out.println("======================================");
 		System.out.println("Total time taken:: "+(end-start)+"ms");
 		System.out.println("======================================");
+		auditTrailSaveService.LogEvent(LogType.GLACE_LOG, LogModuleType.PHYSICALEXAMINATION, LogActionType.VIEW, 0, Log_Outcome.SUCCESS, "successfully retrieved Examination element details", -1, request.getRemoteAddr(), patientId, "chartId="+chartId+"|encounterId"+encounterId+"|templateId="+templateId+"|clientId="+clientId, LogUserType.USER_LOGIN, "", "");
 		return emrResponseBean;
 	}
 	
@@ -87,6 +103,8 @@ public class ExaminationController {
 		templateId=Integer.parseInt(Optional.fromNullable(templateId+"").or("-1"));
 		chartId=Integer.parseInt(Optional.fromNullable(chartId+"").or("-1"));
 		examinationService.markAllNormal(patientId, encounterId, templateId,chartId,systemIds);
+		auditTrailSaveService.LogEvent(LogType.GLACE_LOG, LogModuleType.PHYSICALEXAMINATION, LogActionType.VIEW, 0, Log_Outcome.SUCCESS, "successfully retrieved Mark all normal details", -1, request.getRemoteAddr(), patientId, "chartId="+chartId+"|encounterId"+encounterId+"|templateId="+templateId, LogUserType.USER_LOGIN, "", "");
+		return;
 	}
 	
 	
@@ -103,6 +121,8 @@ public class ExaminationController {
 		templateId=Integer.parseInt(Optional.fromNullable(templateId+"").or("-1"));
 		chartId=Integer.parseInt(Optional.fromNullable(chartId+"").or("-1"));
 		examinationService.clearAllNormal(patientId, encounterId, templateId,chartId,systemIds);
+		auditTrailSaveService.LogEvent(LogType.GLACE_LOG, LogModuleType.PHYSICALEXAMINATION, LogActionType.VIEW, 0, Log_Outcome.SUCCESS, "successfully retrieved clear all normal details", -1, request.getRemoteAddr(), patientId, "chartId="+chartId+"|encounterId"+encounterId+"|templateId="+templateId, LogUserType.USER_LOGIN, "", "");
+		return;
 	}
 	
 	@RequestMapping(value="/PeNotes",method=RequestMethod.GET)
@@ -114,6 +134,7 @@ public class ExaminationController {
 		encounterId=Integer.parseInt(Optional.fromNullable(encounterId+"").or("-1"));
 		EMRResponseBean emrResponseBean = new EMRResponseBean();
 		emrResponseBean.setData(examinationService.getPENotes(patientId,encounterId));
+		auditTrailSaveService.LogEvent(LogType.GLACE_LOG, LogModuleType.PHYSICALEXAMINATION, LogActionType.VIEW, 0, Log_Outcome.SUCCESS, "successfully retrieved PE Notes data", -1, request.getRemoteAddr(), patientId, "encounterId"+encounterId, LogUserType.USER_LOGIN, "", "");
 		return emrResponseBean;
 	}
 	
@@ -134,6 +155,8 @@ public class ExaminationController {
 		System.out.println("======================================");
 		System.out.println("Total time taken:: "+(end-start)+"ms");
 		System.out.println("======================================");
+		
+		auditTrailSaveService.LogEvent(LogType.GLACE_LOG, LogModuleType.PHYSICALEXAMINATION, LogActionType.VIEW, 0, Log_Outcome.SUCCESS, "successfully retrieved Quick Notes data", -1, request.getRemoteAddr(), -1, "", LogUserType.USER_LOGIN, "", "");
 		return emrResponseBean;
 	}
 	
@@ -146,6 +169,7 @@ public class ExaminationController {
 		tabId=Integer.parseInt(Optional.fromNullable(tabId+"").or("-1"));
 		EMRResponseBean emrResponseBean = new EMRResponseBean();
 		emrResponseBean.setData(examinationService.addQuickNotes(tabId,elementId,elementData));
+		auditTrailSaveService.LogEvent(LogType.GLACE_LOG, LogModuleType.PHYSICALEXAMINATION, LogActionType.UPDATE, 0, Log_Outcome.SUCCESS, "successfully added Quick Notes data", -1, request.getRemoteAddr(), -1, "", LogUserType.USER_LOGIN, "", "");
 		return emrResponseBean;
 	}
 	
@@ -156,6 +180,7 @@ public class ExaminationController {
 		dataListId=Integer.parseInt(Optional.fromNullable(dataListId+"").or("-1"));
 		EMRResponseBean emrResponseBean = new EMRResponseBean();
 		emrResponseBean.setData(examinationService.deleteQuickNotes(dataListId));
+		auditTrailSaveService.LogEvent(LogType.GLACE_LOG, LogModuleType.PHYSICALEXAMINATION, LogActionType.DELETE, 0, Log_Outcome.SUCCESS, "successfully deleted Quick Notes data", -1, request.getRemoteAddr(), -1, "", LogUserType.USER_LOGIN, "", "");
 		return emrResponseBean;
 	}
 	
