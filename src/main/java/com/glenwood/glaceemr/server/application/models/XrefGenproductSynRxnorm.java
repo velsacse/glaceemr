42e865ab1de7b739bb535a54b845c6f60313b240
Bhavya011 <bhavya@glenwoodsystems.com>
2017-03-09 13:52:01 +0530

    Handled io serializable exception
    Change-Id: I3c257ca3256b8c4be080f99736e18fddcfa87df7

--- src/main/java/com/glenwood/glaceemr/server/application/models/XrefGenproductSynRxnorm.java
+++ src/main/java/com/glenwood/glaceemr/server/application/models/XrefGenproductSynRxnorm.java
@@ -1,5 +1,7 @@
 package com.glenwood.glaceemr.server.application.models;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -7,7 +9,12 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "xref_genproduct_syn_rxnorm")
-public class XrefGenproductSynRxnorm{
+public class XrefGenproductSynRxnorm implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@Column(name="id")
