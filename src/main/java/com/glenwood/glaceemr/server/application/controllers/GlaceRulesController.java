755ab5c2de0d2c61649a9f87281fdb7594cbb572
Bhavya011 <bhavya@glenwoodsystems.com>
2017-05-11 16:35:55 +0530

    Added audit log for glace rules - Pratheeba
    Change-Id: Ia131a4d97a93c23cff1cbd2708871042e22ec6d6

--- src/main/java/com/glenwood/glaceemr/server/application/controllers/GlaceRulesController.java
+++ src/main/java/com/glenwood/glaceemr/server/application/controllers/GlaceRulesController.java
@@ -1,5 +1,7 @@
 package com.glenwood.glaceemr.server.application.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,6 +9,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.glenwood.glaceemr.server.application.services.audittrail.AuditTrailEnumConstants.LogActionType;
+import com.glenwood.glaceemr.server.application.services.audittrail.AuditTrailEnumConstants.LogModuleType;
+import com.glenwood.glaceemr.server.application.services.audittrail.AuditTrailEnumConstants.LogType;
+import com.glenwood.glaceemr.server.application.services.audittrail.AuditTrailEnumConstants.LogUserType;
+import com.glenwood.glaceemr.server.application.services.audittrail.AuditTrailEnumConstants.Log_Outcome;
+import com.glenwood.glaceemr.server.application.services.audittrail.AuditTrailSaveService;
 import com.glenwood.glaceemr.server.application.services.rulesengine.GlaceRulesService;
 import com.glenwood.glaceemr.server.utils.EMRResponseBean;
 
@@ -23,6 +31,12 @@ public class GlaceRulesController {
 
 	@Autowired
 	GlaceRulesService glaceRulesService;
+	
+	@Autowired
+	AuditTrailSaveService auditTrailSaveService;
+	
+	@Autowired
+	HttpServletRequest request;
 
 	@RequestMapping(value = "/getPQRSMeasure", method = RequestMethod.GET)
     @ResponseBody
@@ -31,6 +45,7 @@ public class GlaceRulesController {
 		String measure=glaceRulesService.getMeasures(providerId);
 		EMRResponseBean result=new EMRResponseBean();
 		result.setData(measure);
+		auditTrailSaveService.LogEvent(LogType.GLACE_LOG, LogModuleType.PQRS, LogActionType.VIEW, 0, Log_Outcome.SUCCESS, "Collected Configured Measures", -1, request.getRemoteAddr(), -1, "", LogUserType.USER_LOGIN, "", "");
 		return result;
 		
 	}
